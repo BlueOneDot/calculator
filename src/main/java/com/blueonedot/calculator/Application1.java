f9eb8157efbbdc3d996277f80e1e4a29ec659c04
euuuuuuan <euan.may24@gmail.com>
2024-05-22 16:34:02 +0900

    불필요한 코드를 삭제하였음.

--- src/main/java/com/blueonedot/calculator/Application1.java
+++ src/main/java/com/blueonedot/calculator/Application1.java
@@ -4,12 +4,5 @@ public class Application1 {
 
     public static void main(String[] args) {
 
-        Parkhayarn hy = new Parkhayarn();
-
-        hy.calculatorPlus1();
-
-
-        Euuuuuuan euan = new Euuuuuuan();
-        euan.calculator();
     }
 }
