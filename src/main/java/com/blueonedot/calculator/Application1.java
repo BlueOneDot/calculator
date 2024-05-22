8e0b1aed17ddf2e87d586648886ed34938813af7
DESKTOP-RTFT54O\80405 <a01027947353@gmail.com>
2024-05-22 17:49:04 +0900

    권은혜의 한 클래스 안에 여러 메소드 호출
    1. calculatorMultip3()
    : 곱하기 계산기
    
    2. calculatorMax()
    : 최대값 구하기
    
    3. calculatorOperator()
    : 연산자 올바르게 선택했는지 감시

--- src/main/java/com/blueonedot/calculator/Application1.java
+++ src/main/java/com/blueonedot/calculator/Application1.java
@@ -1,7 +1,6 @@
 package com.blueonedot.calculator;
 
 public class Application1 {
-    public static void main(String[] args) {
 
-    }
+
 }
--- src/main/java/com/blueonedot/calculator/Dmsgpk.java
+++ src/main/java/com/blueonedot/calculator/Dmsgpk.java
@@ -1,4 +1,81 @@
 package com.blueonedot.calculator;
 
+import java.util.Scanner;
+
 public class Dmsgpk {
+
+    public void calculatorMultip3() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("곱하기 계산기 ver.1 입니다.");
+
+        System.out.println("첫 번째 숫자를 입력하세요. (소수점까지 입력하세요) : ");
+        double value1 = sc.nextDouble();
+
+        System.out.println("두 번째 숫자를 입력하세요. (소수점까지 입력하세요) : ");
+        double value2 = sc.nextDouble();
+
+        System.out.println();
+        double result = 0;
+        result = value1 * value2;
+
+        System.out.println( value1 + " 와 " + value2 + " 의 + 결과는 : " + result + " 입니다.");
+        System.out.println();
+
+    }
+
+    public void calculatorMax() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("정수 두 개를 입력받아 둘 중 더 큰 수를 출력합니다.");
+        System.out.println();
+
+        System.out.println("첫 번째 숫자를 입력하세요 : ");
+        int num1 = sc.nextInt();
+
+        System.out.println();
+
+        System.out.println("두 번째 숫자를 입력하세요 : ");
+        int num2 = sc.nextInt();
+
+        Math.max(num1, num2);
+
+
+        System.out.println("max : " +Math.max(num1, num2));
+
+    }
+
+    public void calculatorOperator() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("연산자를 선택하세요 : ");
+        System.out.println("(연산자 보기 : +, -, *, /, % 중 선택)");
+        char op = sc.next().charAt(0);
+
+        double result = 0;
+
+        switch (op) {
+            case '+':
+                break;
+
+            case '-':
+                break;
+
+            case '*':
+                break;
+
+            case '/':
+                break;
+
+            case '%':
+                break;
+
+            default:
+                System.out.println("연산 기호를 잘못 입력하셨습니다. ");
+
+        }
+
+    }
+
 }
