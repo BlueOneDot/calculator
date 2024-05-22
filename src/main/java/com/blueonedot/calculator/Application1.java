2f17a195b70fe503629fc474811841163716c717
kangyeonjin <yeonjin917n.n@gmail.com>
2024-05-22 13:16:23 +0900

    remain

--- src/main/java/com/blueonedot/calculator/Application1.java
+++ src/main/java/com/blueonedot/calculator/Application1.java
@@ -3,5 +3,8 @@ package com.blueonedot.calculator;
 public class Application1 {
     public static void main(String[] args) {
 
+        Yeonjin yeonjin = new Yeonjin();
+        yeonjin.remain();
+
     }
 }
--- src/main/java/com/blueonedot/calculator/Yeonjin.java
+++ src/main/java/com/blueonedot/calculator/Yeonjin.java
@@ -3,16 +3,19 @@ package com.blueonedot.calculator;
 import java.util.Scanner;
 
 public class Yeonjin {
-    public static void main(String[] args) {
+    public void remain (){
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("첫번째 정수를 입력하세요");
-        sc.nextInt();
+        int num1 = sc.nextInt();
 
-        System.out.println("첫번째 정수를 입력하세요");
-        sc.nextInt();
+        System.out.println("두번째 정수를 입력하세요");
+        int num2 = sc.nextInt();
 
+        int remainder = num1 % num2;
+
+        System.out.println("두 정수를 나눈 나머지는: " + remainder);
 
     }
 
