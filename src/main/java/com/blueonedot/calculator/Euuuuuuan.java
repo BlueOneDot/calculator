3ec2ee44ed37288f98a7cc7da10a55ffb528b7b1
euuuuuuan <euan.may24@gmail.com>
2024-05-22 17:45:45 +0900

    각 기능을 담은 메소드를 분할하였음.

--- src/main/java/com/blueonedot/calculator/Euuuuuuan.java
+++ src/main/java/com/blueonedot/calculator/Euuuuuuan.java
@@ -5,31 +5,39 @@ import java.util.Scanner;
 public class Euuuuuuan {
     public static void main(String[] args) {
         Euuuuuuan eu = new Euuuuuuan();
-        eu.calculator();
+        eu.printIntegers();
     }
 
-    public void calculator() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("연산할 첫번째 숫자를 입력하세요. : ");
-        int num1 = sc.nextInt();
+    public void calculatorMinus() {
+        int[] arr = twoNumbersInput();
 
-        System.out.print("연산할 두번째 숫자를 입력하세요. : ");
-        int num2 = sc.nextInt();
+        int result = arr[0] - arr[1];
 
-        System.out.println("-(마이너스)와 m(최소값) 기호를 입력시 연산 실행");
-
-        int result = 0;
-        char minus = sc.next().charAt(0);
-
-        switch (minus){
-            case '-':
-                result = num1 - num2;
-                break;
-            case 'm':
-                result = Math.min(num1, num2);
-        }
-        
         System.out.println("결과값: " + result);
 
     }
+
+    public void printIntegers() {
+
+        int[] numbers = twoNumbersInput();
+
+        System.out.println("첫번째 숫자 : " + numbers[0]);
+        System.out.println("두번째 숫자 : " + numbers[1]);
+        System.out.println("두 수를 뺀 값 : " + (numbers[0] - numbers[1]));
+    }
+
+
+    public int[] twoNumbersInput() {
+        int[] arr = new int[2];
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("연산할 첫번째 숫자를 입력하세요. : ");
+        arr[0] = sc.nextInt();
+
+        System.out.print("연산할 두번째 숫자를 입력하세요. : ");
+        arr[1] = sc.nextInt();
+        return arr;
+    }
+
+
 }
