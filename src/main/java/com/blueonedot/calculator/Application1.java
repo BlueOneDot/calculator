2f924bd510e7256e7183ff1b05561137d42b5c2b
euuuuuuan <euan.may24@gmail.com>
2024-05-22 14:31:05 +0900

    Class name : Euuuuuuan

--- src/main/java/com/blueonedot/calculator/Application1.java
+++ src/main/java/com/blueonedot/calculator/Application1.java
@@ -3,5 +3,7 @@ package com.blueonedot.calculator;
 public class Application1 {
     public static void main(String[] args) {
 
+        Euuuuuuan euan = new Euuuuuuan();
+        euan.calculator();
     }
 }
