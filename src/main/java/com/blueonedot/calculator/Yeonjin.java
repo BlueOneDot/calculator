8c8ac98dcb8b54b61ada1b901a65e56365f5c638
kangyeonjin <yeonjin917n.n@gmail.com>
2024-05-22 17:30:56 +0900

    randomPrint > remain
    랜덤프린터 불필요로 나누기 연산으로 바꿈

--- src/main/java/com/blueonedot/calculator/Yeonjin.java
+++ src/main/java/com/blueonedot/calculator/Yeonjin.java
@@ -1,10 +1,20 @@
 package com.blueonedot.calculator;
+import java.util.Scanner;
 
 public class Yeonjin {
-   public void randomPrint(){
+    public void remain() {
 
-       int random = (int) (Math.random()*5);
-       System.out.println(random);
-   }
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("첫번째 정수를 입력하세요");
+        int num1 = sc.nextInt();
+
+        System.out.println("첫번째 정수를 입력하세요");
+        int num2 = sc.nextInt();
+
+        int remainder = num1 % num2;
+
+        System.out.println("두 정수를 나눈 나머지는: " + remainder);
+    }
 }
+
