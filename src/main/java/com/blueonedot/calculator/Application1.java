d1c2123b0631a808db26ce914b71c83a19a06d30
DESKTOP-RTFT54O\80405 <a01027947353@gmail.com>
2024-05-24 15:38:29 +0900

    Application 오류를 수정했음.

--- src/main/java/com/blueonedot/calculator/Application1.java
+++ src/main/java/com/blueonedot/calculator/Application1.java
@@ -7,6 +7,16 @@ public class Application1 {
     public static void main(String[] args) {
         System.out.println("청일점 계산기입니다.");
         Scanner sc = new Scanner(System.in);
+
+        Parkhayarn hy = new Parkhayarn(); //하얀님
+        Euuuuuuan euan = new Euuuuuuan(); //유안님
+        Dmsgpk dms = new Dmsgpk();
+        Seongeun se = new Seongeun();
+        Yeonjin yj = new Yeonjin();
+
+
+
+
         while (true) {
 
             System.out.println("원하시는 기능을 입력해주세요");
@@ -17,6 +27,7 @@ public class Application1 {
             System.out.println("%. 두수의 몫을 구하는 기능");
             System.out.println("1. 최대값 구하기");
             System.out.println("2. 최소값 구하기");
+
 //        System.out.println("첫번째 값을 입력하세요.");
             String str = sc.nextLine();
             if (str.equals("exit")) {
@@ -29,36 +40,29 @@ public class Application1 {
 
             switch (str) {
                 case "+":
-                    Parkhayarn hy = new Parkhayarn();
                     hy.calculatorPlus1();
                     break;
                 case "-":
-                    Euuuuuuan euan = new Euuuuuuan();
-                    euan.calculator();
+                    euan.calculatorMinus();
                     break;
                 case "*":
-                    Dmsgpk dms = new Dmsgpk();
                     dms.calculatorMultip3();
                     break;
 
                 case "/":
-                    Seongeun se = new Seongeun();
                     se.calculatorDivide();
                     break;
 
                 case "%":
-                    Yeonjin yj = new Yeonjin();
-                    yj.calculatRemain1();
+                    yj.calculatorRemain1();
                     break;
 
                 case "1":
-                    Dmsgpk dms = new Dmsgpk();
                     dms.calculatorMax();
                     break;
 
                 case "2":
-                    Yeonjin yj = new Yeonjin();
-                    yj.randomPrint();
+                    // yj.randomPrint();
                     break;
 
                 default:
@@ -72,3 +76,4 @@ public class Application1 {
         }
 
     }
+}
